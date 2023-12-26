fe7eaedad7199f85649712ab42b3a1e97353969e
Luis <gerardo.fosado@cargamos.com>
2023-12-26 15:46:49 -0600

    11.-Function

--- src/main.go
+++ src/main.go
@@ -2,30 +2,30 @@ package main
 
 import "fmt"
 
-func main() {
-	// Declaracion de variables
-	helloMessage := "Hello"
-	wordMessage := "word"
-
-	// Printnl
-	fmt.Println(helloMessage, wordMessage)
-	fmt.Println(helloMessage, wordMessage)
-
-	// Printf
-	nombre := "Luis"
-	edad := 20
-	hijos := true
-
-	fmt.Printf("%s tiene mas de %d años \n", nombre, edad)
-	fmt.Printf("%#v tiene mas de %#v años \n", nombre, edad)
-
-	//Sprintf
-	message := fmt.Sprintf("%v tiene mas de %v años", nombre, edad)
-
+func normalFunction(message string) {
 	fmt.Println(message)
+}
 
-	// Tipo de datos
-	fmt.Printf("helloMessage: %T \n", helloMessage)
-	fmt.Printf("hijos: %T \n", hijos)
-	fmt.Printf("edad: %T \n", edad)
+func tripleArguments(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a * 3
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 3
+}
+func main() {
+	normalFunction("hola mundo")
+	tripleArguments(1, 2, "Hola")
+	value := returnValue(2)
+	fmt.Println("value: ", value)
+
+	value1, value2 := doubleReturn(value)
+	fmt.Println("value1 y value2: ", value1, value2)
+
+	value3, _ := doubleReturn(value)
+	fmt.Println("value3: ", value3)
 }
